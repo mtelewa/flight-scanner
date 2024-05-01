5ca6beb39562674ced49aa9eef6a1760cb667a3c
Mohamed Tarek <m.tarek@live.co.uk>
2024-05-01 14:15:56 +0000

    update welcome message and add validation to user input though validate_city function

--- run.py
+++ run.py
@@ -18,20 +18,75 @@ SHEET = GSPREAD_CLIENT.open('interaction_matrix')
 
 
 def display_welcome():
-    welcome_message = Fore.GREEN + '\033[1m' + "Wecome to Flight Scanner!".center(40) + '\033[0m'      # 40 columns will be in deployment terminal center
+    """
+    displays welcome message to the user
+    """
+    welcome_message = \
+        Fore.GREEN + "\033[1m" + "\n" + "********************************".center(40) + \
+                                 "\n" + "** Welcome to FLIGHT SCANNER! **".center(40) + \
+                                 "\n" + "********************************".center(40) + "\n" + "\033[0m"      # 40 columns will be in deployment terminal center
     print(welcome_message)
 
 
-display_welcome()
+# define the cities available in the database
+cities = SHEET.worksheet("jan1").col_values(1)[1:]
 
-a = SHEET.worksheet("jan1").get_all_values()
+def get_departure_city():
+    while True:
+        print(Fore.YELLOW + "\033[1m" + "Where are you travelling from?" + "\033[0m")
+        print("Insert the full city name, first three letters or the number next to the city")
+        print("example: cairo, cai or 2 for Cairo \n")
+        for idx, val in enumerate(cities):
+            print(idx, val)
+        departure_city = input(Fore.BLUE + "\n" + "\033[1m" + "I am travelling from: \n" + "\033[0m")
 
-pprint(a[2][1])
 
-price = a[2][1].split(",")[0]
-print(price)
+        if validate_city(departure_city):
+            city_chosen = cities[validate_city(departure_city)]
+            print(f"You chose {upper(city_chosen)}")
+            break
 
-duration = a[2][1].split(",")[1]
-print(duration)
+
+def validate_city(city):
+    """
+    checks that city is in the database either the full city name
+    or the first three letters or the number
+    """
+    if city in cities:
+        idx = cities.index(city)
+        return idx
+    elif any(item.startswith(city) for item in cities):
+        print('ne')
+    else:
+        try:
+            idx = int(city)
+            return idx
+        except ValueError:
+            print('Invalid city: city is not in database \n')
+            return False
+
+    # else:
+    #     raise ValueError('Invalid city: city is not in database \n')
+    #     return False
+
+
+if __name__ == '__main__':
+    """
+    Run all program functions
+    """
+    display_welcome()
+    get_departure_city()
+
+
+
+# a = SHEET.worksheet("jan1").get_all_values()
+
+# pprint(a[2][1])
+
+# price = a[2][1].split(",")[0]
+# print(price)
+
+# duration = a[2][1].split(",")[1]
+# print(duration)
 
 
