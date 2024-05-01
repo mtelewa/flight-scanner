fa9c53e8322b6ea4c2508a58d95c222ef3e99b33
Mohamed Tarek <m.tarek@live.co.uk>
2024-05-01 12:16:30 +0000

    function to display welcome message

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import gspread 
 from google.oauth2.service_account import Credentials
+from pprint import pprint
+from colorama import Fore, Back, Style
 
 # set scope (lists apis that the program shall access in order to run)
 # It is a constant (that's why capital letters)
@@ -14,5 +16,22 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('interaction_matrix')
 
-a = SHEET.worksheet("january").get_all_values()
-print(a)
+
+def display_welcome():
+    welcome_message = Fore.GREEN + '\033[1m' + "Wecome to Flight Scanner!".center(40) + '\033[0m'      # 40 columns will be in deployment terminal center
+    print(welcome_message)
+
+
+display_welcome()
+
+a = SHEET.worksheet("jan1").get_all_values()
+
+pprint(a[2][1])
+
+price = a[2][1].split(",")[0]
+print(price)
+
+duration = a[2][1].split(",")[1]
+print(duration)
+
+
